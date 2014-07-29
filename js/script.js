654985de1f861c6eb9988f1671bbbc132eb72011
Kelsey Ledford <kelsey.d.ledford@gmail.com>
2014-07-29 09:31:30 -0700

    changes colors and font; adds bootstrap; adds modals

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// $(document).ready(function() {
-
-// 	// Initialize Masonry
-// 	$('#content').masonry({
-// 		columnWidth: 300,
-// 		itemSelector: '.item',
-// 		isFitWidth: true,
-// 		isAnimated: !Modernizr.csstransitions
-// 	}).imagesLoaded(function() {
-// 		$(this).masonry('reload');
-// 	});
-
-// });
-
 var container = document.querySelector('#container');
 var msnry = new Masonry( container, {
   // options
